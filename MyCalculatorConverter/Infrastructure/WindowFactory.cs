9954cb07c899d1b7cedc35b310ef1e1c217a79f7
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-01-10 15:14:54 +0300

    add IWindowFactory

---
+++ MyCalculatorConverter/Infrastructure/WindowFactory.cs
@@ -0,0 +1,21 @@
+﻿using Сalculator_Converter.Infrastructure.Interfaces;
+
+using System.Windows;
+
+namespace Сalculator_Converter.Infrastructure
+{
+    internal sealed class WindowFactory : IWindowFactory
+    {
+		public Window CreateWindow(WindowCreationOptions options)
+		{
+			return new Window()
+			{
+				WindowStartupLocation = WindowStartupLocation.CenterOwner,
+				Title = options.Title,
+				Width = options.WindowSize.Size.Width,
+				Height = options.WindowSize.Size.Height,
+				Owner = Application.Current.MainWindow
+			};
+		}
+	}
+}
