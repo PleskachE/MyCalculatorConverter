03f75b5a6a6f51fa6c2d6ac30d218cd1a7b5d830
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-01-10 15:29:09 +0300

    add NumberConverter

---
+++ MyCalculatorConverter/Converters/NumbersConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace MyCalculatorConverter.Converters
+{
+    public static class NumbersConverter
+    {
+        public static double StringToDoubleConvert(string text)
+        {
+            double result = 0;
+            try
+            {
+                result = Double.Parse(text);
+            }
+            catch
+            {
+                MessageBox.Show("Что то ввелось не так");
+            }
+            return result;
+        }
+
+        public static string DoubleToStringConvert(double value)
+        {
+            string result = "";
+            try
+            {
+                result = value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Что то ввелось не так");
+            }
+            return result;
+        }
+    }
+}
