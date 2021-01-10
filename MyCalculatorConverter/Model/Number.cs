dc96b65ad0802e9fe602554ca9440334a8b6a2ab
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-01-10 15:08:55 +0300

    add model

---
+++ MyCalculatorConverter/Model/Number.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyСalculatorConverter.Model
+{
+    public class Number : INotifyPropertyChanged
+    {
+        private double _value;
+
+        public Number() { _value = 0; }
+
+        public Number(double value)
+        {
+            _value = value;
+        }
+
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        #region operators
+        public static Number operator +(Number x, Number y)
+        {
+            return new Number { Value = x.Value + y.Value };
+        }
+
+        public static Number operator -(Number x, Number y)
+        {
+            return new Number { Value = x.Value - y.Value };
+        }
+
+        public static Number operator *(Number x, Number y)
+        {
+            return new Number { Value = x.Value * y.Value };
+        }
+
+        public static Number operator /(Number x, Number y)
+        {
+            return new Number { Value = x.Value / y.Value };
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
--- MyCalculatorConverter/ViewModel/Abstraction/ViewModelBase.cs
+++ MyCalculatorConverter/ViewModel/Abstraction/ViewModelBase.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace MyСalculatorConverter.ViewModel
+namespace MyСalculatorConverter.ViewModel.Abstraction
 {
     public abstract class ViewModelBase :  INotifyPropertyChanged, IDisposable
     {
