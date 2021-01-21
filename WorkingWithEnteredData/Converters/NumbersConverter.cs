e24a6941680a570f5bcbf26835b2f7712246b1d1
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-01-21 11:31:24 +0300

    edits converters

---
+++ WorkingWithEnteredData/Converters/NumbersConverter.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WorkingWithEnteredData.Converters
+{
+    public class NumbersConverter
+    {
+        public double StringToDouble(string text)
+        {
+            double result = 0;
+            double.TryParse(text, out result);
+            return result;
+        }
+    }
+}
