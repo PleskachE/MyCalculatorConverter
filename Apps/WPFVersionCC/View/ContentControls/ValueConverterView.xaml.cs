e2890ff8a7a7c8d30993eb7f2463dd7500d5c079
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-02-24 23:48:10 +0300

    Add ValueConverterView

---
+++ Apps/WPFVersionCC/View/ContentControls/ValueConverterView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyCalculatorConverter.View.ContentControls
+{
+    /// <summary>
+    /// Логика взаимодействия для ValueConverterView.xaml
+    /// </summary>
+    public partial class ValueConverterView : ContentControl
+    {
+        public ValueConverterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
