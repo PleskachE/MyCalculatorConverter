95aeb4d9836d30f51aa6b12b884d0cc0fc6284bb
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-03-02 20:59:05 +0300

    Add Common

---
+++ Common/MemberInfoExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace Common
+{
+    public static class MemberInfoExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this MemberInfo self, bool inherit = false)
+            where TAttribute : Attribute
+        {
+            return (TAttribute)Attribute.GetCustomAttribute(self, typeof(TAttribute), inherit);
+        }
+    }
+}
