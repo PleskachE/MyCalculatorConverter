70bd371460e621a5ef8a68efb284f2f80d1ee312
PleskachE <50796641+PleskachE@users.noreply.github.com>
2021-04-09 15:21:21 +0300

    A new executer - NumberConverter has been added to BLL.

---
+++ Bll/Executers/NumberConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+using Bll.Executers.Abstractions;
+
+using NLog;
+
+using NumberSystemConverter.abstraction;
+
+namespace Bll.Executers
+{
+    public class NumberConverter : IExecuter
+    {
+        #region Fields
+
+        private INumberConverter _converter;
+        private Logger _logger;
+
+        #endregion
+
+        #region Ctor
+
+        public NumberConverter(INumberConverter converter)
+        {
+            _converter = converter;
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string Calculation(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                sb.Append(_converter.Conversion(text));
+            }
+            catch(Exception ex)
+            {
+                _logger.Error(ex);
+                sb.Append("0");
+            }
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
